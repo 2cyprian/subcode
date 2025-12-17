6531794fb63591bd8b49268c49d47e34d72205b3
2cyprian <cyprianodindo@gmail.com>
2025-12-17 23:46:38 +0300

    Enhance HTML structure and CSS styles for improved responsiveness and asset path corrections

--- src/js/components/card.js
+++ src/js/components/card.js
@@ -8,32 +8,32 @@
 // 1. The Data: Add or edit services here
 const servicesData = [
     {
-        icon: "../assets/icons/digital.svg",
+        icon: "src/assets/icons/digital.svg",
         title: "Digital Transformation",
         description: "Transform your business with technology that automates processes and creates cloud experiences, starting with an analysis of your technological and business landscape."
     },
     {
-        icon: "../assets/icons/front.svg",
+        icon: "src/assets/icons/front.svg",
         title: "Front & Backend Design",
         description: "We offer UI/UX design and front-end development services that create user-friendly, visually appealing, and functional digital products."
     },
     {
-        icon: "../assets/icons/web.svg", // Represents Web/Mobile/Desktop
+        icon: "src/assets/icons/web.svg", // Represents Web/Mobile/Desktop
         title: "Web, Mobile & Desktop App Development",
         description: "Deliver stunning, intuitive mobile and desktop applications that engage customers with expert development services that meet their demands."
     },
     {
-        icon: "../assets/icons/saas.svg",
+        icon: "src/assets/icons/saas.svg",
         title: "SaaS App Development",
         description: "Collect and analyze your daily customer data to make informed business decisions, because bold companies know innovation is the path to success."
     },
     {
-        icon: "../assets/icons/cd.svg",
+        icon: "src/assets/icons/cd.svg",
         title: "CI/CD, support and maintenance",
         description: "We provide CI/CD solutions for seamless delivery, plus support and maintenance to keep your software secure and up-to-date."
     },
     {
-        icon: "../assets/icons/ai.svg",
+        icon: "src/assets/icons/ai.svg",
         title: "Automations, A.I & Machine Learning",
         description: "We specialize in automation, AI, and machine learning solutions that streamline processes, enable informed decisions, and drive efficiency."
     }
--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -3,15 +3,15 @@ const featuredProjectsDB = [
     {
         id: "Tech",
         label: "Tech",
-        icon: "../assets/icons/tech.svg", 
+        icon: "src/assets/icons/tech.svg", 
         // PROJECT DETAILS
         title: "Tehama Awards Tanzania",
         description: "The TEHAMA Awards is Tanzania’s premier ICT recognition platform. We built a comprehensive digital nomination and voting system that streamlined the entire process, ensuring transparency and real-time data analytics for the judges.",
         technologies: [
-            { icon: "../assets/icons/python.svg", color: "#3776AB" },
-            { icon: "../assets/icons/react.svg", color: "#61DAFB" },
-            { icon: "../assets/icons/docker.svg", color: "#2496ED" },
-            { icon: "../assets/icons/mysql.svg", color: "#333" }
+            { icon: "src/assets/icons/python.svg", color: "#3776AB" },
+            { icon: "src/assets/icons/react.svg", color: "#61DAFB" },
+            { icon: "src/assets/icons/docker.svg", color: "#2496ED" },
+            { icon: "src/assets/icons/mysql.svg", color: "#333" }
         ],
         testimonial: {
             text: "Subcode transformed our digital presence with exceptional results.",
@@ -21,25 +21,25 @@ const featuredProjectsDB = [
         },
         images: {
             // Using the Tehama images
-            desktopBack: "../assets/projects/tehamaweb1.png",
-            desktopFront: "../assets/projects/tehamaweb2.png",
-            mobileLeft: "../assets/projects/tehamaphone1.jpg",
-            mobileRight: "../assets/projects/tehamaphone2.jpg"
+            desktopBack: "src/assets/projects/tehamaweb1.png",
+            desktopFront: "src/assets/projects/tehamaweb2.png",
+            mobileLeft: "src/assets/projects/tehamaphone1.jpg",
+            mobileRight: "src/assets/projects/tehamaphone2.jpg"
         },
         link:"https://tehamaawards.tz/"
     },
     {
         id: "finance",
         label: "Finance",
-        icon: "../assets/icons/finance.svg",
+        icon: "src/assets/icons/finance.svg",
         // PROJECT DETAILS
         title: "M-Pesa Gateway Integration",
         description: "A robust payment gateway solution designed for high-volume transactions. We integrated seamless mobile money APIs, fraud detection systems, and a merchant dashboard that processes thousands of transactions daily.",
         technologies: [
-            { icon: "../assets/icons/fastapi.svg", color: "#E76F00" },
-            { icon: "../assets/icons/vue.svg", color: "#F7DF1E" },
-            { icon: "../assets/icons/docker.svg", color: "#2496ED" },
-            { icon: "../assets/icons/postgres.svg", color: "#333" },
+            { icon: "src/assets/icons/fastapi.svg", color: "#E76F00" },
+            { icon: "src/assets/icons/vue.svg", color: "#F7DF1E" },
+            { icon: "src/assets/icons/docker.svg", color: "#2496ED" },
+            { icon: "src/assets/icons/postgres.svg", color: "#333" },
             ],
         testimonial: {
             text: "Security and speed were our top priorities, and <br>SubCode delivered beyond expectations.",
@@ -49,7 +49,7 @@ const featuredProjectsDB = [
         },
         images: {
             // Finance placeholders
-            desktopBack: "../assets/projects/binaryweb1.png",
+            desktopBack: "src/assets/projects/binaryweb1.png",
             desktopFront: "https://images.unsplash.com/photo-1563986768609-322da13575f3?auto=format&fit=crop&w=800&q=80",
             mobileLeft: "https://images.unsplash.com/photo-1556742031-c6961e8560b0?auto=format&fit=crop&w=300&q=80",
             mobileRight: "https://images.unsplash.com/photo-1611162617474-5b21e879e113?auto=format&fit=crop&w=300&q=80"
@@ -58,15 +58,15 @@ const featuredProjectsDB = [
     {
         id: "telecom",
         label: "Telecom",
-        icon: "../assets/icons/telecom.svg",
+        icon: "src/assets/icons/telecom.svg",
         // PROJECT DETAILS
         title: "Network Monitor Dashboard",
         description: "An enterprise-grade visualization tool for tracking network uptime and signal strength across regions. This platform provides real-time alerts and predictive maintenance schedules for telecom engineers.",
         technologies: [
-            { icon: "../assets/icons/nextjs.svg", color: "#DD0031" },
-            { icon: "../assets/icons/django.svg", color: "#68A063" },
-            { icon: "../assets/icons/mongodb.svg", color: "#555" },
-            { icon: "../assets/icons/linux.svg", color: "#8D7646" }
+            { icon: "src/assets/icons/nextjs.svg", color: "#DD0031" },
+            { icon: "src/assets/icons/django.svg", color: "#68A063" },
+            { icon: "src/assets/icons/mongodb.svg", color: "#555" },
+            { icon: "src/assets/icons/linux.svg", color: "#8D7646" }
         ],
         testimonial: {
             text: "Our response time to outages improved by <br>40% thanks to this dashboard.",
@@ -85,15 +85,15 @@ const featuredProjectsDB = [
     {
         id: "healthcare",
         label: "Healthcare",
-        icon: "../assets/icons/health.svg",
+        icon: "src/assets/icons/health.svg",
         // PROJECT DETAILS
         title: "AfyaConnect Telemed",
         description: "A patient-doctor connectivity app allowing remote consultations, prescription management, and secure medical history storage. Built with HIPAA compliance standards for maximum data privacy.",
         technologies: [
-            { icon: "../assets/icons/fastapi.svg", color: "#F05138" },
-            { icon: "../assets/icons/android.svg", color: "#3DDC84" },
-            { icon: "../assets/icons/tailwindcss.svg", color: "#333" },
-            { icon: "../assets/icons/mysql.svg", color: "#8D7646" }
+            { icon: "src/assets/icons/fastapi.svg", color: "#F05138" },
+            { icon: "src/assets/icons/android.svg", color: "#3DDC84" },
+            { icon: "src/assets/icons/tailwindcss.svg", color: "#333" },
+            { icon: "src/assets/icons/mysql.svg", color: "#8D7646" }
         ],
         testimonial: {
             text: "This app has made healthcare accessible to rural<br> communities like never before.",
@@ -112,15 +112,15 @@ const featuredProjectsDB = [
     {
         id: "energy",
         label: "Energy",
-        icon: "../assets/icons/energy.svg",
+        icon: "src/assets/icons/energy.svg",
         // PROJECT DETAILS
         title: "Rashal Energies Website",
         description: "A modern, fast, and fully responsive corporate website built for Rashal Energies to showcase their solar products, services, and clean-energy solutions. The platform strengthens their brand presence, improves customer trust, and makes it easy for clients to explore and contact the company.",
         technologies: [
-            { icon: "../assets/icons/bulma.svg", color: "#4caf50" },
-            { icon: "../assets/icons/python.svg", color: "#3776AB" },
-            { icon: "../assets/icons/react.svg", color: "#333" },
-            { icon: "../assets/icons/tailwindcss.svg", color: "#FBBF24" }
+            { icon: "src/assets/icons/bulma.svg", color: "#4caf50" },
+            { icon: "src/assets/icons/python.svg", color: "#3776AB" },
+            { icon: "src/assets/icons/react.svg", color: "#333" },
+            { icon: "src/assets/icons/tailwindcss.svg", color: "#FBBF24" }
         ],
         testimonial: {
             text: "A clean, modern website that clearly communicates <br> our services and strengthens our brand presence.",
@@ -130,25 +130,25 @@ const featuredProjectsDB = [
         },
         images: {
             // Energy placeholders
-            desktopBack: "../assets/projects/rashalweb1.png",
-            desktopFront: "../assets/projects/rashalweb2.png",
-            mobileLeft: "../assets/projects/rashalphone1.png",
-            mobileRight: "../assets/projects/rashalphone2.png"
+            desktopBack: "src/assets/projects/rashalweb1.png",
+            desktopFront: "src/assets/projects/rashalweb2.png",
+            mobileLeft: "src/assets/projects/rashalphone1.png",
+            mobileRight: "src/assets/projects/rashalphone2.png"
         },
         link:"https://www.rashalenergies.co.tz"
     },
     {
         id: "Education",
         label: "Education",
-        icon: "../assets/icons/education.svg",
+        icon: "src/assets/icons/education.svg",
         // PROJECT DETAILS
         title: "Learning & Training Portal",
         description: "A modern, interactive e-learning platform designed to help students and professionals access courses, track progress, and build practical skills. The system includes quizzes, progress<br> analytics, and a responsive user experience for seamless learning.",
         technologies: [
-            { icon: "../assets/icons/html.svg", color: "#E44D26" },
-            { icon: "../assets/icons/css.svg", color: "#1572B6" },
-            { icon: "../assets/icons/vue.svg", color: "#F7DF1E" },
-            { icon: "../assets/icons/node.svg", color: "#3C873A" }
+            { icon: "src/assets/icons/html.svg", color: "#E44D26" },
+            { icon: "src/assets/icons/css.svg", color: "#1572B6" },
+            { icon: "src/assets/icons/vue.svg", color: "#F7DF1E" },
+            { icon: "src/assets/icons/node.svg", color: "#3C873A" }
         ],
         testimonial: {
             text: "The platform transformed how our learners access training <br> and track their growth.",
@@ -158,10 +158,10 @@ const featuredProjectsDB = [
         },
         images: {
             // Tech placeholders
-            desktopBack: "../assets/projects/binaryweb1.png",
-            desktopFront: "../assets/projects/binaryweb2.png",
-            mobileLeft: "../assets/projects/binaryphone1.png",
-            mobileRight: "../assets/projects/binaryphone2.png"
+            desktopBack: "src/assets/projects/binaryweb1.png",
+            desktopFront: "src/assets/projects/binaryweb2.png",
+            mobileLeft: "src/assets/projects/binaryphone1.png",
+            mobileRight: "src/assets/projects/binaryphone2.png"
         }
     }
 ];
